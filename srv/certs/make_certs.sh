b847a4c970a308adc34666715dba9ba39e014f8a
anna <anna@localhost.localdomain>
2022-01-29 10:52:41 -0500

    first commit

---
+++ srv/certs/make_certs.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+ 
+#####echo "Create Signing Key and CSR"
+openssl req -nodes -newkey rsa:2048 -keyout registry-auth.key -out registry-auth.csr -subj "/CN=git.labs.lc"
+#####echo "Self-Sign Certificate"
+openssl x509 -in registry-auth.csr -out registry-auth.crt -req -signkey registry-auth.key -days 3650
+ 
+openssl genrsa -out rootCA.key 2048
+openssl req -x509 -new -nodes -key rootCA.key -days 1024 -out rootCA.pem
+openssl genrsa -out registry.key 2048
+openssl req -new -key registry.key -out registry.csr
+###### Common Name
+####Common Name (eg, YOUR name) []: registry.git.labs.lc
+openssl x509 -req -in registry.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out registry.crt -days 500
